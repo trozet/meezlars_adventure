32099df1d651652430cf6f66e767f5c7034ae2bf
Bernie Parker <Bernie Parker>
2016-04-02 02:31:29 -0400

    Retrying commit

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button playButton;
+    public Button exitButton;
+    public Text yesQuit;
+    public Text noQuit;
+
+	public void Start ()
+    {
+        quitMenu.enabled = false;
+	}
+
+    public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        playButton.enabled = false;
+        exitButton.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        quitMenu.enabled = false;
+        playButton.enabled = true;
+        exitButton.enabled = true;
+    }
+	
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+   
+}
